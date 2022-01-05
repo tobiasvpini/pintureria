7e539f911ca468d03ebbb703c8ce97cb2b462636
Tobias <tobiasvpini@gmail.com>
2022-01-05 01:23:01 +0100

    fixed scroll and some style changes

--- frontend/src/view/handleScroll.js
+++ frontend/src/view/handleScroll.js
@@ -1,17 +1,17 @@
 export default function checkOffset() {
     const form = document.querySelector("#form-container");
 
-    if((window.innerHeight + window.scrollY ) > document.body.offsetHeight - 155) {
-        form.style.position = "absolute";
-        form.style.top = "2020px"
-        console.log("holaabs")
-    } else{
-        form.style.position = "fixed";
-        form.style.top = form.scrollTop + 243 + "px";      
-    }
-      
-    
-    
-    
-}
+    if(window.matchMedia("(min-width: 1025px)").matches){
 
+        if((window.innerHeight + window.scrollY ) > document.body.offsetHeight - 200) {
+            form.style.position = "absolute";
+            form.style.top = "1695px";
+        } else if(window.scrollY < 1900){
+            form.style.position = "fixed";
+            form.style.top = form.scrollTop + 247 + "px";    
+            console.log(window.innerHeight - form.offsetTop + form.offsetHeight) 
+        } 
+          
+    } 
+
+}
