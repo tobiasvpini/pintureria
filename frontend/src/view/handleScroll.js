12181fd2495e31a7e4616242113ea96f1fad74ae
Tobias <tobiasvpini@gmail.com>
2022-01-10 22:42:43 +0100

    empty service-container out and handleScroll() fixed

--- frontend/src/view/handleScroll.js
+++ frontend/src/view/handleScroll.js
@@ -5,11 +5,13 @@ export default function checkOffset() {
 
         if((window.innerHeight + window.scrollY ) > document.body.offsetHeight - 200) {
             form.style.position = "absolute";
-            form.style.top = "1695px";
-        } else if(window.scrollY < 1900){
+            form.style.top = "1415px";
+            console.log("estoy aca")
+        } else if(window.scrollY < 1800){
             form.style.position = "fixed";
-            form.style.top = form.scrollTop + 247 + "px";    
-            console.log(window.innerHeight - form.offsetTop + form.offsetHeight) 
+            form.style.top = form.scrollTop + 245.5 + "px";    
+            console.log(window.scrollY) 
+            console.log("xd bro")
         } 
           
     } 
