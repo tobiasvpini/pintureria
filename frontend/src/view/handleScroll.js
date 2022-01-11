807b4f1a30877e74a6eb2af2b6f9633270b31d67
Tobias <tobiasvpini@gmail.com>
2022-01-12 00:13:53 +0100

    fixing styles

--- frontend/src/view/handleScroll.js
+++ frontend/src/view/handleScroll.js
@@ -6,12 +6,9 @@ export default function checkOffset() {
         if((window.innerHeight + window.scrollY ) > document.body.offsetHeight - 200) {
             form.style.position = "absolute";
             form.style.top = "1500px";
-            console.log("estoy aca")
         } else if(window.scrollY < 1750){
             form.style.position = "fixed";
             form.style.top = form.scrollTop + 246.8 + "px";    
-            console.log(window.scrollY) 
-            console.log("xd bro")
         } 
           
     } 
